23ea7ced6839bbcde9714cc5121ed5e129213310
ValeLeon15 <vlg1529@gmail.com>
2024-03-05 10:54:44 -0500

    base sql

---
+++ src/main/java/com/example/demo/repository/ArrendatarioRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.demo.entity.Arrendatario;
+
+public interface ArrendatarioRepository  extends CrudRepository<Arrendatario,Long>{
+
+}
