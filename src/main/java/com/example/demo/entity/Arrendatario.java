df2f399940f17aeb5539cbdb313150b49c3e7573
ValeLeon15 <vlg1529@gmail.com>
2024-03-14 15:52:59 -0500

    kd

--- src/main/java/com/example/demo/entity/Arrendatario.java
+++ src/main/java/com/example/demo/entity/Arrendatario.java
@@ -4,8 +4,6 @@ import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,12 +15,9 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Where(clause = "status = 0")
-@SQLDelete(sql = "UPDATE application SET  status = 1 WHERE id=?")
 public class Arrendatario {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private long uuid;
     private String nombre;
     private String cedula;
     
