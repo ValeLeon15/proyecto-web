92a201ece1095716971976883a3047f3932dbcbb
ValeLeon15 <vlg1529@gmail.com>
2024-03-14 15:53:56 -0500

    d

--- src/main/java/com/example/demo/entity/Arrendatario.java
+++ src/main/java/com/example/demo/entity/Arrendatario.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Arrendatario {
     @Id
-    private long uuid;
+    private long id;
     private String nombre;
     private String cedula;
     
