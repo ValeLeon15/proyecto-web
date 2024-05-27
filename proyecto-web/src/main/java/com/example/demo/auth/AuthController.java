160377b3b5c10b4a076b7af6dd85c6296bca6253
limosnd <123989336+limosnd@users.noreply.github.com>
2024-05-27 18:29:14 -0500

    ultimo

--- proyecto-web/src/main/java/com/example/demo/auth/AuthController.java
+++ proyecto-web/src/main/java/com/example/demo/auth/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Type;
-
+//mejorar
 @RestController
 @RequestMapping(value="/progrupo14/auth")
 @CrossOrigin(origins = "http://localhost")
